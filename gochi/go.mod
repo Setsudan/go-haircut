784b3e20e18db35d89da002af0d419d32dd6d474
Setsudashi <cryptiareta@gmail.com>
2024-01-18 14:04:28 +0100

    🗃️ : Real db init

--- gochi/go.mod
+++ gochi/go.mod
@@ -1,5 +1,10 @@
-module gochifood
+module gohairdresser
 
 go 1.21.3
 
 require github.com/go-chi/chi/v5 v5.0.11
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- gochi/main.go
+++ gochi/main.go
@@ -1,41 +1,21 @@
 package main
 
 import (
-	"encoding/json"
+	"fmt"
 	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"gohairdresser/database"
+	"gohairdresser/router"
 )
 
-// Res struct
-type Res struct {
-	Code    int         `json:"code"`
-	Status  string      `json:"status"`
-	Message string      `json:"message"`
-	Data    interface{} `json:"data"`
-}
-
 func main() {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
+	r := router.SetupRouter()
 
-	// CORS
-	r.Use(middleware.SetHeader("Access-Control-Allow-Origin", "*"))
-	r.Use(middleware.SetHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"))
-	r.Use(middleware.SetHeader("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization"))
+	db := database.SetupDatabase()
+	defer db.Close()
 
-	serverStatusRes := Res{
-		Code:    200,
-		Status:  "OK",
-		Message: "Server is running",
-		Data:    nil,
-	}
+	database.ShowTables(db)
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(serverStatusRes)
-	})
-
+	fmt.Println("Server is running on port 8080")
 	http.ListenAndServe(":8080", r)
 }
