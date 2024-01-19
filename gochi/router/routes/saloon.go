2ffe0383619d3a78632d2635bf29faa0da53bd7f
Setsudashi <cryptiareta@gmail.com>
2024-01-19 15:37:31 +0100

    ✏️ : fix merge post apocalypse

--- gochi/router/routes/saloon.go
+++ gochi/router/routes/saloon.go
@@ -15,60 +15,7 @@ func SaloonRoutes(r *chi.Mux) {
 		r.Post("/create", createSaloonRoute)
 		r.Get("/all", getAllHairSaloons)
 		r.Get("/{uid}", getHairSaloonByUID)
-		r.Post("/create", createSaloonRoute)
 	})
-}
-
-func createSaloonRoute(w http.ResponseWriter, r *http.Request) {
-	var saloon structs.CreateHairSaloon
-
-	// Decode the body into the struct
-	err := json.NewDecoder(r.Body).Decode(&saloon)
-	if err != nil {
-		if err == io.EOF {
-			// Handle empty body
-			response := structs.APIResponse{
-				Code:    http.StatusBadRequest,
-				Status:  "error",
-				Message: "Request body is empty or in wrong format",
-				Data:    structs.CreateHairSaloon{}, // Provide an example of the expected format
-			}
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(response)
-			return
-		}
-		// Handle other JSON decoding errors
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	// Insert the saloon into the database
-	createdSaloon, err := database.CreateSaloon(saloon)
-	if err != nil {
-		response := structs.APIResponse{
-			Code:    http.StatusInternalServerError,
-			Status:  "error",
-			Message: "Failed to create saloon",
-			Data:    nil,
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(response)
-		return
-	}
-
-	// Successful response
-	response := structs.APIResponse{
-		Code:    http.StatusCreated,
-		Status:  "success",
-		Message: "Saloon created successfully",
-		Data:    createdSaloon,
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(response)
-
 }
 
 func getAllHairSaloons(w http.ResponseWriter, r *http.Request) {
